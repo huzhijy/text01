cb2c78d3971d56f615c1c674917fab29d2a07024
huzhi <UID@uid-PC>
2019-08-12 15:00:54 +0800

    new file

---
+++ helloworlk/src/main/java/com/tedu0/HelloworlkApplication.java
@@ -0,0 +1,13 @@
+package com.tedu0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworlkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworlkApplication.class, args);
+	}
+
+}
